5e5fef74b46fbf7ec6f0823271c99edd5bb66fdc
dong881 <joe888joejoe@gmail.com>
2024-10-13 17:33:14 +0800

    Fix padding size in fillRarPdu function and update RAR_PAYLOAD_SIZE constant

--- l2/src/cm/mac_sch_interface.h
+++ l2/src/cm/mac_sch_interface.h
@@ -97,7 +97,7 @@
 #define MAX_NUM_DL_DATA_TO_UL_ACK 15
 #define QPSK_MODULATION 2
 
-#define RAR_PAYLOAD_SIZE 10             /* As per spec 38.321, sections 6.1.5 and 6.2.3, RAR PDU is 8 bytes long and 2 bytes of padding */
+#define RAR_PAYLOAD_SIZE 11             /* As per spec 38.321, sections 6.1.5 and 6.2.3, RAR PDU is 8 bytes long and 2 bytes of padding */
 #define TX_PAYLOAD_HDR_LEN 0           /* Intel L1 requires adding a 32 byte header to transmitted payload */
 #define UL_TX_BUFFER_SIZE 5
 
