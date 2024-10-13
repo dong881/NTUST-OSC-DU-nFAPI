3d713d763009c774bd8887af09e562f5f61afd7e
dong881 <joe888joejoe@gmail.com>
2024-10-13 17:15:35 +0800

    Develop new function: getMcsTable(uint16_t mcs, uint8_t colIdx) This function retrieves a value from the MCS table based on the MCS index and column index.

--- l2/src/5gnrsch/sch_utils.h
+++ l2/src/5gnrsch/sch_utils.h
@@ -119,6 +119,7 @@ uint8_t calculateSlotPatternLength(uint8_t scs, uint8_t periodicity);
 #endif
 
 /* Functions declarations : Resource allocation handler */
+uint16_t getMcsTable(uint16_t mcs, uint8_t colIdx);
 uint16_t schCalcTbSize(uint32_t payLoadSize);
 uint16_t schCalcNumPrb(uint16_t tbSize, uint16_t mcs, uint8_t numSymbols);
 uint16_t schCalcTbSizeFromNPrb(uint16_t numPrb, uint16_t mcs, uint8_t numSymbols);
